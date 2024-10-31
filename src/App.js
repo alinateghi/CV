0cdfd50486d7cc3ae3a7d2f2dad50107dcbde38e
Ali Nateghi <nateghi.dev@gmail.com>
2024-10-31 12:14:19 +0330

    structure and base codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Education from './components/Education/Education';
+import Experience from './components/Experience/Experience';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className='nav'>
+        <Nav />
+      </div>
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='jobs'>
+        <Experience />
+      </div>
+      <div className='edj'>
+        <Education />
+      </div>
     </div>
   );
 }
